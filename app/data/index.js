c24e7790968ec814bcb5f65b87f63629a4c985a7
bryant <bryantkrause@gmail.com>
2019-11-21 06:50:49 -0800

    able to post data to array need to look at json

---
+++ app/data/index.js
@@ -0,0 +1,1 @@
+// user data all about them lets export that outa here
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,10 +1,17 @@
 
 
+let people = [{
+    name: "testBro",
+    photo:  "https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwjO-6-xsPrlAhWLvZ4KHacKAc8QjRx6BAgBEAQ&url=https%3A%2F%2Fspongebob.fandom.com%2Fwiki%2FSpongeBob_SquarePants_(character)&psig=AOvVaw2X52MHQRwBs_6FvzAQg41F&ust=1574393853300297",
+    scores:[1,2,3,4,5,6,7,8,9,10]
+}]
+
+
 module.exports = app => {
 
     // route to display all potential friends
 app.get('/friends',(req, res) => {
-    console.log('ping')
+    console.log(people)
     res.sendStatus(200)
 
     })
@@ -12,7 +19,8 @@ app.get('/friends',(req, res) => {
 
     // route to input survey data
     app.post('/friends', (req, res) => {
-        console.log('ping')
+        console.log(req.body)
+        people.push(req.body)
     res.sendStatus(200)
     // route is working need to take user survey and add it to "giant list" of other users to see if they are compatible... or not.
     })
