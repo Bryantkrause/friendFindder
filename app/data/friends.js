fbe7de424fa8d972f027a730d972b062009df977
bryant <bryantkrause@gmail.com>
2019-11-20 20:44:48 -0800

    getting organized

---
+++ app/data/friends.js
@@ -0,0 +1,11 @@
+// user data all about them lets export that outa here
+
+
+module.exports = app => {
+let people = [{
+    name: "testBro",
+    photo:  "https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwjO-6-xsPrlAhWLvZ4KHacKAc8QjRx6BAgBEAQ&url=https%3A%2F%2Fspongebob.fandom.com%2Fwiki%2FSpongeBob_SquarePants_(character)&psig=AOvVaw2X52MHQRwBs_6FvzAQg41F&ust=1574393853300297",
+    scores:[1,2,3,4,5,6,7,8,9,10]
+}]
+
+}
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -14,6 +14,7 @@ app.get('/friends',(req, res) => {
     app.post('/friends', (req, res) => {
         console.log('ping')
     res.sendStatus(200)
+    // route is working need to take user survey and add it to "giant list" of other users to see if they are compatible... or not.
     })
 }
 
