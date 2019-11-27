9d1af65b182c251a7f2b7ae12b8eb9df8a6073e6
Bryant Krause <bkrause@ttdwest.local>
2019-11-27 15:45:41 -0800

    used a dictionary could not find definition of:

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,16 +1,14 @@
 
-
-let people = [{
-    name: "testBro",
-    photo:  "https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwjO-6-xsPrlAhWLvZ4KHacKAc8QjRx6BAgBEAQ&url=https%3A%2F%2Fspongebob.fandom.com%2Fwiki%2FSpongeBob_SquarePants_(character)&psig=AOvVaw2X52MHQRwBs_6FvzAQg41F&ust=1574393853300297",
-    scores:[1,2,3,4,5,6,7,8,9,10]
-}]
-
+// should theoretically import the array of people
+require('../app/data/index')
 
 module.exports = app => {
 
     // route to display all potential friends
 app.get('/friends',(req, res) => {
+    console.log(people[0].name)
+    console.log(people[0].photo)
+    console.log(people[0].scores)
     console.log(people)
     res.sendStatus(200)
 
