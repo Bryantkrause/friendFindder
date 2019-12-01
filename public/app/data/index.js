0d42e1236fc7cc3b9a79caf002e6e0874fd2fd7e
bryant <bryantkrause@gmail.com>
2019-12-01 14:07:39 -0800

    working questionator

--- public/app/data/index.js
+++ public/app/data/index.js
@@ -1,13 +1,33 @@
 // user data all about them lets export that outa here
-let people = [{
-    name: "testBro",
-    photo:  "https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwjO-6-xsPrlAhWLvZ4KHacKAc8QjRx6BAgBEAQ&url=https%3A%2F%2Fspongebob.fandom.com%2Fwiki%2FSpongeBob_SquarePants_(character)&psig=AOvVaw2X52MHQRwBs_6FvzAQg41F&ust=1574393853300297",
-    scores:[1,2,3,4,5,6,7,8,9,10]
-},{
-	name: "brotest",
-	photo: "photo.com",
-	scores: [1,2,3,4,5,6,7,8,9,10]
-}]
+let questions = ['How do you feel about how much time you spend together?',
+    'Would you say that your partner understands what you are feeling?',
+    'How important is it for you to equally divvy up chores?',
+    'How much alone time do you need?',
+    'How content are you with the amount of non-sexual affection in the relationship?',
+    'The two of you are hitting the great outdoors. What would you prefer to spend the day doing?',
+    'What would you prefer to do on a Saturday night?',
+    'How would you rate your kissing?',
+    'Do you think you and your partner should share the same values?',
+    'Do you think a couple’s finances should be together or separate?']
+
+console.log(questions)
+console.log('this is an index')
+
+let survey = document.getElementById('submit').addEventListener('click', e => {
+    e.preventDefault()
+
+    questions.forEach(function (x){
+console.log(x)
+let tr = document.createElement('tr')
+tr.id = x
+tr.innerHTML = `
+<td>${x}</td>
+`
+document.getElementById('survey').append(tr)
+    })
+    })
+
+
 
 // // function to recieve user survey and then compare it 
 // document.getElementById('submit').addEventListener('click', e => {
@@ -21,6 +41,3 @@ let people = [{
 
 
 // should theoretically export the array of people
-module.exports = {
-people
-}
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,6 +1,14 @@
 
 // should theoretically import the array of people
-let people = require('../public/app/data/')
+let people = [{
+    name: "testBro",
+    photo:  "https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwjO-6-xsPrlAhWLvZ4KHacKAc8QjRx6BAgBEAQ&url=https%3A%2F%2Fspongebob.fandom.com%2Fwiki%2FSpongeBob_SquarePants_(character)&psig=AOvVaw2X52MHQRwBs_6FvzAQg41F&ust=1574393853300297",
+    scores:[1,2,3,4,5,6,7,8,9,10]
+},{
+	name: "brotest",
+	photo: "photo.com",
+	scores: [1,2,3,4,5,6,7,8,9,10]
+}]
 
 module.exports = app => {
 
